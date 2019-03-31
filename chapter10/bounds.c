d3064c6e472332d079d96aeba8cd6f81589e25c7
Yau <manfred3431@outlook.com>
2019-03-31 16:39:15 +0800

    Finish chapter9.

---
+++ chapter10/bounds.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define SIZE 4
+int main(void)
+{
+    int value1 = 44;
+    int arr[SIZE];
+    int value2 = 88;
+    int i;
+
+    printf("value1 = %d, value2 = %d\n", value1, value2);
+    for
+}
