135267486169faf696336b9e36a46919ebff4d8a
Manfred <manfred3431@outlook.com>
2019-04-14 09:27:44 +0800

    A

---
+++ chapter11/getsputs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define STLEN 81
+int main(void)
+{
+    char a[10];
+    gets(a);
+    gets(a);
+    
+//    puts("我是中國人");
+//    char words[STLEN];
+//
+//    puts("Enter a string, please.");
+//    gets(words);
+//    printf("Your string twice:\n");
+//    printf("%s\n", words);
+//    puts(words);
+//    puts("Done.");
+
+    return 0;
+}
