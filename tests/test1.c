bd274c1da525a831a5f084656d0e7eccf9031a26
manfred3431@outlook.com <manfred3431@outlook.com>
2019-05-21 18:09:52 +0800

    Chapter12

--- tests/test1.c
+++ tests/test1.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-int main(void) {
-    char s1[20] = "abcde";
-//    for (int i = 0; i < 5; ++i) {
-//        s1[i] = 'A';
-//    }
-//    s1[5] = '\0';
-    char s2[] = "NICE";
-    strncpy(s1, s2, 4);
+#include <time.h>
 
-    for (int j = 0; s1[j] != '\0'; ++j) {
-        printf("%c", s1[j]);
-    }
+int main(void) {
+    int i;
+    char c;
+    scanf("%d", &i);
+
+    while ((c = getchar()) != '\n')
+        printf("%c ", c);
+
     return 0;
 }
 
