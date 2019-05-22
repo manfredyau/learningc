f710a95ba93e0b992915802dd8dfbc4f92cfd707
manfred3431@outlook.com <manfred3431@outlook.com>
2019-05-22 19:08:19 +0800

    Chapter12

--- tests/test1.c
+++ tests/test1.c
@@ -2,15 +2,19 @@
 #include <string.h>
 #include <ctype.h>
 #include <time.h>
+#include <stdlib.h>
+int abc = 9;
+int main(void)
+{
+    FILE *fp;
+    fp = fopen("file.txt", "r+");
 
-int main(void) {
-    int i;
-    char c;
-    scanf("%d", &i);
-
-    while ((c = getchar()) != '\n')
-        printf("%c ", c);
-
+    putchar(getc(fp));
+    putchar(getc(fp));
+    ungetc('H', fp);
+//    fseek(fp, 0L, SEEK_SET);
+//    putchar(getc(fp));
+    putchar(getc(fp));
     return 0;
 }
 
