478ffad40165d9df42937af677116add5cafa586
manfred3431@outlook.com <manfred3431@outlook.com>
2019-05-24 12:02:38 +0800

    Chapter 14 first

--- tests/test1.c
+++ tests/test1.c
@@ -3,12 +3,24 @@
 #include <ctype.h>
 #include <time.h>
 #include <stdlib.h>
+
+struct person
+{
+    int age;
+    char name[50];
+};
+
 int main(void)
 {
-    int arr[] = {1,2,3,4,5};
-    void *arr2 = arr;
-    printf("%zd\n", sizeof(void) );
-    printf("%d\n", arr2 + 1 );
+    struct person one;
+    one = (struct person){
+            21,
+            "Manfred"
+    };
+
+    int arr[2];
+    arr = (int[2]) {10, 20};
     return 0;
 }
+
 
